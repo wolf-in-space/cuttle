6a9c6c6ef9f7692ed8f5fd68cbd33f17cf01459d
wolf-in-space <jakob@wolf-in-space.dev>
2024-09-02 11:27:08 +0200

    replace system for setting the flag with an observer and add a remove observer as well

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -6,7 +6,7 @@ use crate::{
     flag::{BitPosition, CompFlag},
     shader::{CompShaderInfo, CompShaderInfos},
     utils::GetOrInitResourceWorldExt,
-    ComdfExtractSet, ComdfPostUpdateSet,
+    ComdfExtractSet,
 };
 use bevy::{prelude::*, render::RenderApp};
 use std::any::type_name;
@@ -29,12 +29,11 @@ impl RegisterSdfRenderCompAppExt for App {
         let world = self.world_mut();
         let mut infos = world.resource_or_init::<CompShaderInfos>();
         let bit_index = infos.register(C::shader_info());
-        world.insert_resource(BitPosition::<C>::new(bit_index));
 
-        self.add_systems(
-            PostUpdate,
-            C::set_flag_bit.in_set(ComdfPostUpdateSet::BuildFlag),
-        );
+        world.insert_resource(BitPosition::<C>::new(bit_index));
+        world.observe(set_flag_bit::<C, OnAdd, true>);
+        world.observe(set_flag_bit::<C, OnRemove, false>);
+
         self.sub_app_mut(RenderApp).add_systems(
             ExtractSchedule,
             extract_sdf_comp::<C>.in_set(ComdfExtractSet::Extract),
@@ -51,12 +50,22 @@ impl RegisterSdfRenderCompAppExt for App {
     }
 }
 
-pub trait RenderSdfComponent: Sized + Component + Clone {
-    fn set_flag_bit(mut query: Query<&mut CompFlag, With<Self>>, comp_bit: Res<BitPosition<Self>>) {
-        query.iter_mut().for_each(|mut flag| {
-            flag.set(comp_bit.position as usize, true);
-        });
+pub fn set_flag_bit<COMP: Component, T, const SET: bool>(
+    trigger: Trigger<T, COMP>,
+    bit: Res<BitPosition<COMP>>,
+    mut flags: Query<&mut CompFlag>,
+) {
+    if let Ok(mut flag) = flags.get_mut(trigger.entity()) {
+        flag.set(bit.position as usize, SET)
     }
+}
+
+pub trait RenderSdfComponent: Sized + Component + Clone {
+    // fn set_flag_bit(mut query: Query<&mut CompFlag, With<Self>>, comp_bit: Res<BitPosition<Self>>) {
+    //     query.iter_mut().for_each(|mut flag| {
+    //         flag.set(comp_bit.position as usize, true);
+    //     });
+    // }
 
     fn shader_info() -> CompShaderInfo;
 
