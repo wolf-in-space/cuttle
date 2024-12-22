76b4bafc8770be84d30ba4a337cc78c501b61b2c
wolf-in-space <jakob@wolf-in-space.dev>
2024-12-22 11:54:18 +0100

    fix and adjust stress test

--- examples/stress_test.rs
+++ examples/stress_test.rs
@@ -8,41 +8,38 @@ fn main() {
         .run();
 }
 
-const AMOUNT: u32 = 1000;
-
-const RATIO: u32 = 3;
-const X_AMOUNT: u32 = (AMOUNT / 10) * (10 - RATIO);
-const Y_AMOUNT: u32 = (AMOUNT / 10) * RATIO;
+const AXIS_AMOUNT: u32 = 1000;
+const AMOUNT: u32 = AXIS_AMOUNT * AXIS_AMOUNT;
 const X_SIZE: f32 = 1200.;
 const X_HALF_SIZE: f32 = X_SIZE / 2.;
 const Y_SIZE: f32 = 700.;
 const Y_HALF_SIZE: f32 = Y_SIZE / 2.;
-const X_DISTANCE: f32 = X_SIZE / X_AMOUNT as f32;
-const Y_DISTANCE: f32 = Y_SIZE / Y_AMOUNT as f32;
+const X_DISTANCE: f32 = X_SIZE / AXIS_AMOUNT as f32;
+const Y_DISTANCE: f32 = Y_SIZE / AXIS_AMOUNT as f32;
 
 fn spawn(mut cmds: Commands) {
     cmds.spawn(Camera2d);
     let mut entities = Vec::with_capacity(AMOUNT as usize);
-    println!("SPAWNED_CIRCLES={}", X_AMOUNT * Y_AMOUNT);
-    for x in 0..X_AMOUNT {
-        for y in 0..Y_AMOUNT {
+    for x in 0..AXIS_AMOUNT {
+        for y in 0..AXIS_AMOUNT {
             let (x, y) = (x as f32, y as f32);
             entities.push((
                 Sdf,
                 Transform::from_xyz(
                     x * X_DISTANCE - X_HALF_SIZE,
                     y * Y_DISTANCE - Y_HALF_SIZE,
-                    x * y,
+                    0.,
                 ),
-                builtins::Circle { radius: 1. },
+                builtins::Circle { radius: 0.5 },
                 Fill(Srgba::new(
                     f32::sin(x / 100.) + 0.5,
                     f32::cos(y / 100.) + 0.5,
                     0.,
-                    0.,
+                    1.,
                 )),
             ));
         }
     }
+    println!("SPAWNED_CIRCLES={}", AMOUNT);
     cmds.spawn_batch(entities);
 }
